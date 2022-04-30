c5566e7e51ef593fff0f446959f64607b7ac48f9
Adem Emre <ademeg_42@hotmail.com>
2022-04-30 15:50:15 +0300

    add lotties

---
+++ lib/core/constant/app_assets.dart
@@ -0,0 +1,5 @@
+class AppAssets{
+  static const booksLottie = 'assets/lotties/books.json';
+  static const fishLottie = 'assets/lotties/fish.json';
+  static const registerLottie = 'assets/lotties/register.json';
+}
